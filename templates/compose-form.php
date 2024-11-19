137199527ad3b4ae5728ed1d87717507562a6c4e
Md. Aminur Islam <eng.aminurislam@gmail.com>
2024-11-19 16:48:34 +0600

    Ensure proper escaping

--- templates/compose-form.php
+++ templates/compose-form.php
@@ -1,38 +1,38 @@
 <form action="" method="POST" class="wlb-compose-plugin-form <?php echo esc_attr( $attr['class'] ?? '' ); ?>">
     <div class="form-group">
-        <label class="control-label" for="plugin_name">Plugin Name *</label>
+        <label class="control-label" for="plugin_name"><?php echo esc_html__( 'Plugin Name *', 'plugin-composer' ); ?></label>
         <div class="input-group">
-            <input name="plugin_name" id="plugin_name" required class="input-control" placeholder="My Plugin">
-            <div class="error-message"> <?php echo $error_messages['plugin_name'] ?? ''; ?></div>
+            <input name="plugin_name" id="plugin_name" required class="input-control" placeholder="<?php echo esc_attr__( 'My Plugin', 'plugin-composer' ); ?>">
+            <div class="error-message"><?php echo esc_html( $error_messages['plugin_name'] ?? '' ); ?></div>
         </div>
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_description">Plugin Description</label>
-        <textarea name="plugin_description" id="plugin_description"  rows="2" class="input-control" placeholder="Plugin desc."></textarea>
+        <label class="control-label" for="plugin_description"><?php echo esc_html__( 'Plugin Description', 'plugin-composer' ); ?></label>
+        <textarea name="plugin_description" id="plugin_description"  rows="2" class="input-control" placeholder="<?php echo esc_attr__( 'Plugin desc.', 'plugin-composer' ); ?>"></textarea>
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_requires">Requires Plugins</label>
-        <input name="plugin_requires" id="plugin_requires" class="input-control" placeholder="e.i woocommerce, dokan-lite, dokan-pro">
+        <label class="control-label" for="plugin_requires"><?php echo esc_html__( 'Requires Plugins', 'plugin-composer' ); ?></label>
+        <input name="plugin_requires" id="plugin_requires" class="input-control" placeholder="<?php echo esc_attr( 'e.i woocommerce, dokan-lite, dokan-pro' ); ?>">
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_license">Plugin License</label>
-        <input name="plugin_license" id="plugin_license" class="input-control" placeholder="License e.i GPL2">
+        <label class="control-label" for="plugin_license"><?php echo esc_html__( 'Plugin License', 'plugin-composer' ); ?></label>
+        <input name="plugin_license" id="plugin_license" class="input-control" placeholder="<?php echo esc_attr__( 'License e.g., GPL2', 'plugin-composer' ); ?>">
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_uri">Plugin URL</label>
-        <input name="plugin_uri" id="plugin_uri" type="url" class="input-control" placeholder="https://company.com/my-plugin" value="">
+        <label class="control-label" for="plugin_uri"><?php echo esc_html__( 'Plugin URL', 'plugin-composer' ); ?></label>
+        <input name="plugin_uri" id="plugin_uri" type="url" class="input-control" placeholder="<?php echo esc_url( 'https://company.com/my-plugin' ); ?>" value="">
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_author_name">Author Name</label>
-        <input name="plugin_author_name" id="plugin_author_name"  class="input-control" placeholder="weLabs">
+        <label class="control-label" for="plugin_author_name"><?php echo esc_html__( 'Author Name', 'plugin-composer' ); ?></label>
+        <input name="plugin_author_name" id="plugin_author_name"  class="input-control" placeholder="<?php echo esc_attr__( 'weLabs', 'plugin-composer' ); ?>">
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_author_email">Author Email</label>
-        <input name="plugin_author_email" id="plugin_author_email" type="email" class="input-control" placeholder="contact@welabs.dev">
+        <label class="control-label" for="plugin_author_email"><?php echo esc_html__( 'Author Email', 'plugin-composer' ); ?></label>
+        <input name="plugin_author_email" id="plugin_author_email" type="email" class="input-control" placeholder="<?php echo esc_attr( 'contact@welabs.dev' ); ?>">
     </div>
     <div class="form-group">
-        <label class="control-label" for="plugin_author_uri">Author URL</label>
-        <input name="plugin_author_uri" id="plugin_author_uri" type="url"  class="input-control" placeholder="https://author.profile">
+        <label class="control-label" for="plugin_author_uri"><?php echo esc_html__( 'Author URL', 'plugin-composer' ); ?></label>
+        <input name="plugin_author_uri" id="plugin_author_uri" type="url"  class="input-control" placeholder="<?php echo esc_url( 'https://author.profile' ); ?>">
     </div>
 
     <?php wp_nonce_field( 'wlb-compose-plugin', 'wlb-compose-plugin' ); ?>
